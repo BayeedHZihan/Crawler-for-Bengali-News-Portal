87131fca057f41d33b63e1c3b828eec160acb297
Bayeed <bayeed@algomatrix.co>
2020-02-24 03:43:47 +0600

    all set

--- postscrape/postscrape/spiders/posts_spider.py
+++ postscrape/postscrape/spiders/posts_spider.py
@@ -2,12 +2,13 @@ import scrapy
 import hashlib
 import json
 
+
 class PostsSpider(scrapy.Spider):
     name = "posts"
     start_urls = [
         'https://bangla.bdnews24.com/'
     ]
-    direc = ''
+
 
     def parse(self, response):
         #get all the article links
@@ -15,9 +16,8 @@ class PostsSpider(scrapy.Spider):
         
         #follow link one by one 
         for post in post_link_list:
-            #hashed_url = self.hashcodes(post).upper()
-            #self.direc = hashed_url + '.json'
             yield scrapy.Request(post, callback=self.parse_post)
+
 
     def parse_post(self, response):
         items = {}
@@ -25,15 +25,16 @@ class PostsSpider(scrapy.Spider):
         items['date'] = response.css('#article_notations span:nth-child(2)::text').get(),
         items['content'] = response.css('#storyBody p::text , .article_lead_text .print-only::text').extract()
         
-        #I'll probably have to change this part
-        # with open(self.direc, 'w', encoding="utf-8") as f:
-        #     f.write(str(items))
+        #get the current page url and encode it 
         hashed_url = self.hashcodes(response.request.url).upper()
         direc = hashed_url + '.json'
+
+        #write single page scraped data onto individual files
         with open(direc, 'w', encoding='utf-8') as f:
             json.dump(items, f, ensure_ascii=False)
         
         yield items
+
 
     def hashcodes(self, url):
         hc = hashlib.md5(url.encode())
